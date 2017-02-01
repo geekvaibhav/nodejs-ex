36e33f8e94729335f060cd6533c41454e76bf6bf
Vaibhav <geekvaibhav@users.noreply.github.com>
2017-02-01 12:38:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,103 +5,44 @@ var express = require('express'),
     eps     = require('ejs'),
     morgan  = require('morgan');
     
-Object.assign=require('object-assign')
+/* module imports */
+const BotConnector = require('recastai-botconnector')
+const recastai = require('recastai')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+/* Bot Connector connection */
+const myBot = new BotConnector({ userSlug: 'geekvaibhav', botId: '588b3523e807fe2c951faa2f', userToken: 'f7e0e721c3a1de2efb5378f8e7044b73' })
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+/* Recast.AI API connection */
+const client = new recastai.Client('a7a706351177d232d7c1efca6cb6e9d6')
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+/* Server setup */
+const app = express()
+const port = 5000
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+app.use(bodyParser.json())
+app.post('/', (req, res) => myBot.listen(req, res))
+app.listen(port, () => console.log('Bot running on port', port))
 
-  }
-}
-var db = null,
-    dbDetails = new Object();
+/* When a bot receive a message */
+myBot.onTextMessage(message => {
+  console.log(message)
+  const userText = message.content.attachment.content
+  const conversationToken = message.senderId
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+  client.textConverse(userText, { conversationToken })
+    .then(res => {
+      // We get the first reply from Recast.AI or a default reply
+      const reply = res.reply() || 'Sorry, I didn\'t understand'
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
+      const response = {
+        type: 'text',
+        content: reply,
+      }
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+      return message.reply(response)
+    })
+    .then(() => console.log('Message successfully sent'))
+    .catch(err => console.error(`Error while sending message: ${err}`))
+})
